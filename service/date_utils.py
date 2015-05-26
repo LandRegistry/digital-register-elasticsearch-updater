2e251baaf7d137bb24fa81a91980d8aae4f91344
Nick Gravgaard <nick@beautifulcode.net>
2015-05-26 18:37:21 +0100

    Make sure last_modification_date is a datetime object

--- service/date_utils.py
+++ service/date_utils.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 
 def format_date_with_millis(date):
-    # In order to get milliseconds, the last three characters are removed
+    # Convert from microseconds to milliseconds by removing the last 3 chars
     time = date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-    zone = '00'
-    return '{}+{}'.format(time, zone)
+    zone = '+0000'
+    return '{}{}'.format(time, zone)
+
+
+def string_to_date(datetime_string):
+    return datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%S.%f%z')
